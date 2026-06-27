7c0b1661bd5d8458271d78884a6970d334ff6437
decentral1se <cellarspoon@riseup.net>
2026-06-27 13:51:05 +0200

    feat: aarch64 go p2panda_ffi

--- go/p2panda_ffi/cgo_flags.go
+++ go/p2panda_ffi/cgo_flags.go
@@ -1,4 +1,5 @@
 package p2panda_ffi
 
 // #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/libs/x86_64-unknown-linux-musl -lp2panda_ffi -lm
+// #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/libs/aarch64-unknown-linux-musl -lp2panda_ffi -lm
 import "C"
